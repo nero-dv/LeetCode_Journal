dd84a39dd9f490d98520960c228cec8ad1954e39
nero.dv <louis_noir@protonmail.com>
2022-06-17 10:30:56 -0500

    1

---
+++ two_sum.py
@@ -0,0 +1,35 @@
+def twoSum(nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        left = 0
+        right = 1
+        
+        a_len = len(nums)
+        
+        if a_len == 2 and nums[0] + nums[1] == target:
+            return [0, 1]
+        if a_len == 2 and nums[0] + nums[1] != target:
+            return []
+        
+        
+        
+        while left < a_len:
+            if right == a_len:
+                left += 1
+                right = left + 1
+            if nums[left] + nums[right] == target:
+                return [left, right]
+            else:
+                right += 1
+        return []
+    
+testcase = [2,7,11,15]
+target = 9
+# [3,2,4]
+# 6
+# [3,3]
+# 6
